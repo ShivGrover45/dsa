e8227789c25e41e19d680f57ce15d6c12b482b22
ShivGrover45 <codershiv16@gmail.com>
2025-08-15 11:02:01 +0530

    leetcode problem no 283

--- movezeros.java
+++ movezeros.java
@@ -1,3 +1,36 @@
-public  {
-    
+import java.util.Scanner;
+
+public class movezeros {
+
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        moveZero(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] +" "  );
+        }
+    }
+    public static void  moveZero(int[] arr){ //using two pointer approach 
+        int j=-1;
+        //finding the first zero
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1) return ;
+        for(int i=j+1;i<arr.length;i++){
+            if(arr[i]!=0){
+                int temp=arr[i];
+                 arr[i]=arr[j];
+                 arr[j]=temp;
+                j++;
+            }
+        }
+    }
 }
